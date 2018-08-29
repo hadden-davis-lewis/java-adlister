17471c6d059f07c90e342ec0b40d02f2515ef9a1
Amos Davis <amos.davis367@gmail.com>
2018-08-28 20:30:04 -0500

    added additional properties to MySQLUsersDao - insert method - and added addt'l getters and setters to user model

--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -4,9 +4,14 @@ import com.codeup.adlister.util.Password;
 
 public class User {
     private long id;
-    private String username;
+    private String first_name;
+    private String last_name;
     private String email;
+    private String username;
     private String password;
+    private String location;
+    private String profile_pic;
+    private String user_since;
 
     public User() {}
 
@@ -53,5 +58,48 @@ public class User {
 
     public void setPassword(String password) {
         this.password = Password.hash(password);
+    }
+
+
+
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getProfile_pic() {
+        return profile_pic;
+    }
+
+    public void setProfile_pic(String profile_pic) {
+        this.profile_pic = profile_pic;
+    }
+
+    public String getUser_since() {
+        return user_since;
+    }
+
+    public void setUser_since(String user_since) {
+        this.user_since = user_since;
     }
 }
