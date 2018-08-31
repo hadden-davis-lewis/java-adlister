c90e12591d0a4f98f1ea359dd149aa03f89b3337
charleshadden <charles.r.hadden@gmail.com>
2018-08-31 14:03:57 -0500

    made sure user needs to be logged in before he sees ads

--- src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
@@ -13,6 +13,10 @@ import java.io.IOException;
 public class AdsIndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //all users
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
 
         request.setAttribute("user", DaoFactory.getUsersDao().all());
         request.setAttribute("ads", DaoFactory.getAdsDao().all());
@@ -21,11 +25,13 @@ public class AdsIndexServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         req.setAttribute("ads",DaoFactory.getAdsDao().getAdByTitle(req.getParameter("searchString")));
-
         req.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(req, resp);
+    }
+
+
 
 
     }
-}
+
 
 //login required
